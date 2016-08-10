5e422b054f616bad2e89a7f657e21afc05b90ce8
allen <370787716@qq.com>
2016-08-10 15:46:54 +0800

    test
    test

---
+++ chuanzhi/chuanzhi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chuanzhi
+//
+//  Created by allen_Chan on 16/8/10.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chuanzhi/chuanzhi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chuanzhi
+//
+//  Created by allen_Chan on 16/8/10.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
